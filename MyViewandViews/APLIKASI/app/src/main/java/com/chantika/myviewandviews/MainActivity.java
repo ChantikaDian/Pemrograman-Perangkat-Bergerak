4c367336dec573eef04360d4dce203fdb9fd3485
ChantikaDian <chantikadian04@gmail.com>
2019-10-09 08:27:11 +0700

    Upload Job 4

--- MyViewandViews/APLIKASI/app/src/main/java/com/chantika/myviewandviews/MainActivity.java
+++ MyViewandViews/APLIKASI/app/src/main/java/com/chantika/myviewandviews/MainActivity.java
@@ -1,9 +1,13 @@
 package com.chantika.myviewandviews;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+Button BtnDonasi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
 
         if(getSupportActionBar() !=null);
         getSupportActionBar().setTitle("Donasi Pesawat R80");
+
+        BtnDonasi=findViewById(R.id.BtnDonasi);
+        BtnDonasi.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.BtnDonasi:
+                Intent donasi = new Intent(MainActivity.this, Activity2.class);
+                startActivity(donasi);
+                break;
+        }
     }
 }
